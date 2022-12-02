9263fffdaa9663845c87dc1b6fdd92ef61dc89d2
teevik <teemu.vikoren@gmail.com>
2022-12-02 03:36:26 +0100

    Update day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,21 +1,13 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
-pub struct Elf {
-    calories: u32
-}
-
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<Elf> {
-    fn parse_calories_block(input: &str) -> Elf {
-        let calories = input
+pub fn input_generator(input: &str) -> Vec<u32> {
+    fn parse_calories_block(input: &str) -> u32 {
+        input
             .split("\n")
             .map(|input| input.parse::<u32>().unwrap())
-            .sum();
-
-        Elf {
-            calories
-        }
+            .sum()
     }
 
     input.split("\n\n")
@@ -24,17 +16,16 @@ pub fn input_generator(input: &str) -> Vec<Elf> {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(elves: &[Elf]) -> u32 {
-    elves.iter()
-        .map(|elf| elf.calories)
+pub fn solve_part1(all_calories: &[u32]) -> u32 {
+    *all_calories.iter()
         .max()
         .unwrap()
+
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(elves: &[Elf]) -> u32 {
-    elves.iter()
-        .map(|elf| elf.calories)
+pub fn solve_part2(all_calories: &[u32]) -> u32 {
+    all_calories.iter()
         .sorted()
         .rev()
         .take(3)
