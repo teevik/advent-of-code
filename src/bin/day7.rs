6e6fe0ced7fc8f2246fbf8e0cc93c039821a874e
teevik <teemu.vikoren@gmail.com>
2022-12-08 02:47:10 +0100

    aaa

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,119 +1,113 @@
-#![feature(is_some_and)]
-#![feature(hash_raw_entry)]
-
 use advent_of_code::execution_time;
-use anyhow::{Context, Result};
-use slotmap::{new_key_type, SecondaryMap, SlotMap};
-use std::collections::HashMap;
+use anyhow::Result;
 
-new_key_type! { struct DirectoryKey; }
-
-#[derive(Debug)]
-struct Directories<'a> {
-    directories: SlotMap<DirectoryKey, Directory<'a>>,
-    root_directory: DirectoryKey,
-    directory_sizes: SecondaryMap<DirectoryKey, u64>,
+struct Node {
+    value: u64,
+    parent: Option<usize>,
+    children: Vec<usize>,
 }
 
-impl<'a> Directories<'a> {
-    pub fn new() -> Self {
-        let mut directories = SlotMap::with_key();
-        let root_directory = directories.insert(Directory::default());
+impl Node {
+    fn is_directory(&self) -> bool {
+        self.children.len() > 0
+    }
+}
 
-        Self {
-            directories,
-            root_directory,
-            directory_sizes: Default::default(),
+struct FileTree {
+    files: Vec<Node>,
+}
+
+impl FileTree {
+    fn insert(&mut self, value: u64, parent: Option<usize>) -> Option<usize> {
+        let index = self.files.len();
+        if let Some(parent) = parent {
+            self.files[parent].children.push(index);
+            self.update_size(Some(parent), value);
+        }
+        self.files.push(Node {
+            value,
+            parent,
+            children: vec![],
+        });
+        Some(index)
+    }
+
+    fn node(&self, index: Option<usize>) -> &Node {
+        match index {
+            Some(index) => &self.files[index],
+            None => panic!("Trying to access a non-existing node"),
         }
     }
 
-    pub fn add_files_size(&mut self, path: &[&'a str], files_size: u64) {
-        let mut parent_directory_key = self.root_directory;
-
-        let directory_size = self
-            .directory_sizes
-            .entry(parent_directory_key)
-            .unwrap()
-            .or_default();
-
-        *directory_size += files_size;
-
-        for &path_part in path {
-            let directory_key = {
-                let directory = self.directories.get_mut(parent_directory_key).unwrap();
-
-                directory.sub_directories.get(path_part).copied()
-            };
-
-            let directory_key = directory_key.unwrap();
-
-            let directory_size = self
-                .directory_sizes
-                .entry(directory_key)
-                .unwrap()
-                .or_default();
-
-            *directory_size += files_size;
+    fn update_size(&mut self, index: Option<usize>, size: u64) {
+        let mut current = index;
+        while let Some(index) = current {
+            let node = &mut self.files[index];
+            node.value += size;
+            current = node.parent;
         }
     }
-
-    pub fn insert(path: &[&'a str]) {}
 }
 
-#[derive(Default, Debug)]
-struct Directory<'a> {
-    pub sub_directories: HashMap<&'a str, DirectoryKey>,
+const MAX_DIR_SIZE: u64 = 100000;
+const TOTAL_DISK_SIZE: u64 = 70000000;
+const UPDATE_SIZE: u64 = 30000000;
+
+fn parse_file_tree(input: &str) -> FileTree {
+    let mut file_tree = FileTree { files: vec![] };
+    let mut current: Option<usize> = None;
+
+    input.lines().for_each(|line| {
+        let splited: Vec<&str> = line.split(" ").collect();
+        match splited[0] {
+            "$" => match splited[1] {
+                "cd" => match splited[2] {
+                    ".." => {
+                        current = file_tree.node(current).parent;
+                    }
+                    _ => {
+                        current = file_tree.insert(0, current);
+                    }
+                },
+                _ => {}
+            },
+            "dir" => {}
+            _ => {
+                file_tree.insert(splited[0].parse::<u64>().unwrap(), current);
+            }
+        }
+    });
+
+    file_tree
 }
 
 pub fn solve_part1(input: &str) -> Result<u64> {
-    let mut lines = input.lines().peekable();
+    let file_tree = parse_file_tree(input);
 
-    let mut path = Vec::new();
-    let mut directories = Directories::new();
-
-    while lines.peek().is_some() {
-        let line = lines.next().unwrap();
-
-        if let Some(cd) = line.strip_prefix("$ cd ") {
-            match cd {
-                "/" => {
-                    path.clear();
-                }
-                ".." => {
-                    path.pop();
-                }
-                move_to => {
-                    path.push(move_to);
-                }
-            }
-        } else if line.strip_prefix("$ ls").is_some() {
-            let mut files_size = 0;
-
-            while lines.peek().is_some_and(|line| !line.starts_with('$')) {
-                let line = lines.next().unwrap();
-
-                let first_word = line.split(" ").next().context("Parsing error")?;
-
-                if first_word == "dir" {
-                    continue;
-                };
-
-                let size = first_word.parse::<u64>()?;
-
-                files_size += size;
-            }
-
-            directories.insert(&path, files_size);
-        }
-    }
-
-    dbg!(&directories);
+    let directories = file_tree
+        .files
+        .into_iter()
+        .filter(|node| node.is_directory());
 
     let result = directories
-        .directory_sizes
-        .values()
-        .filter(|&&directory_size| directory_size <= 100000)
+        .filter(|node| node.value <= MAX_DIR_SIZE)
+        .map(|node| node.value)
         .sum();
+
+    Ok(result)
+}
+
+pub fn solve_part2(input: &str) -> Result<u64> {
+    let file_tree = parse_file_tree(input);
+
+    let directories = file_tree.files.iter().filter(|node| node.is_directory());
+
+    let to_delete_size = UPDATE_SIZE - (TOTAL_DISK_SIZE - file_tree.files[0].value);
+    let result = directories
+        .filter(|node| node.value >= to_delete_size)
+        .map(|node| node.value)
+        .min()
+        .unwrap();
 
     Ok(result)
 }
@@ -121,11 +115,11 @@ pub fn solve_part1(input: &str) -> Result<u64> {
 pub fn main() -> Result<()> {
     let input = include_str!("../input/day7.txt");
 
-    let part_1 = execution_time(|| solve_part1(input));
+    let part_1 = execution_time(|| solve_part1(input))?;
     dbg!(part_1);
 
-    // let part_2 = execution_time(|| solve_part2(input))?;
-    // dbg!(part_2);
+    let part_2 = execution_time(|| solve_part2(input))?;
+    dbg!(part_2);
 
     Ok(())
 }
